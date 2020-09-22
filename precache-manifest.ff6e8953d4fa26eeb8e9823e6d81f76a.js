5a679f60a12ce7ffe4b14557e45793a2f492135c
Ankit Dedhia <ankit.dedhia@learningmatemumbai.local>
2020-09-22 11:52:52 +0530

    Updates

---
+++ precache-manifest.ff6e8953d4fa26eeb8e9823e6d81f76a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259ef1e7ff09f7e4883194efdb4620d3",
+    "url": "/react-chat-app/index.html"
+  },
+  {
+    "revision": "ed87fdff33c83f8e4ae6",
+    "url": "/react-chat-app/static/css/main.c6cc8795.chunk.css"
+  },
+  {
+    "revision": "8168bd2c026c38270dfc",
+    "url": "/react-chat-app/static/js/2.83544179.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-chat-app/static/js/2.83544179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed87fdff33c83f8e4ae6",
+    "url": "/react-chat-app/static/js/main.7576d57d.chunk.js"
+  },
+  {
+    "revision": "efd3442585b4b34ee6c8",
+    "url": "/react-chat-app/static/js/runtime-main.1aae6afd.js"
+  }
+]);
